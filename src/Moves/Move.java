c900d118620e50252e9ba0175e8294404b696e93
Dhruv Manani <dhruvmanani2000@gmail.com>
2018-01-09 13:03:40 -0500

    Move

--- src/Moves/Move.java
+++ src/Moves/Move.java
@@ -5,16 +5,23 @@ public class Move {
 	String type;
 	int pp;
 	Image move;
+	int accuracy;
 
-	public Move(String type, int pp, Image move) {
+	public Move(String type, int pp, int accuracy, Image move) {
 		this.type = type;
 		this.pp = pp;
 		this.move = move;
+		this.accuracy = accuracy;
 	}
 
 	public int calculateDamage(Pokemon pokemon){
 	return 0;
 	}
+
 	public void makeMove(Pokemon pokemon){
 	}
 }
+public static class fireBall extends Move{
+	fireBall
+}
+
