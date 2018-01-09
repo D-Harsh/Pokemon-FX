f0f8a017c8e98b8368058bfbea7fc91e4f594d04
D-Harsh <harsh200014@gmail.com>
2018-01-09 13:00:46 -0500

    changed pokemon stuff

--- src/Pokemon/Pokemon.java
+++ src/Pokemon/Pokemon.java
@@ -1,24 +1,24 @@
 package Pokemon;
+
 import Moves.Move;
 import javafx.scene.image.Image;
+
 /**
  * Created by Harsh on 2018-01-08.
  */
 
 public class Pokemon {
     String name, type;
-    int hP, healthStat, speedStat, stengthStat;
+    int hP, speedStat, healthIV, speedIV;
+    double stengthStat, strengthIV;
     Move move0, move1, move2, move3;
     Image pokemon;
 
-
-
-    public Pokemon(String name, String type, int hP, int healthStat, int speedStat, int stengthStat, Move move0,
-                   Move move1, Move move2, Move move3, Image pokemon) {
+    public Pokemon(String name, String type, int hP, int speedStat, double stengthStat,
+                   Move move0, Move move1, Move move2, Move move3, Image pokemon) {
         this.name = name;
         this.type = type;
         this.hP = hP;
-        this.healthStat = healthStat;
         this.speedStat = speedStat;
         this.stengthStat = stengthStat;
         this.move0 = move0;
@@ -26,9 +26,25 @@ public class Pokemon {
         this.move2 = move2;
         this.move3 = move3;
         this.pokemon = pokemon;
+        generateIVs();
+    }
+
+
+
+    public void generateIVs() {
+        this.healthIV = (int)(Math.random() * 31);
+        this.speedIV = (int)(Math.random() * 31);
+        this.strengthIV = (Math.random() * 0.5);
     }
 
     public void useMove(Pokemon opponent, Move move) {
         move.makeMove(opponent);
+    }
+
+    public static class Charizad extends Pokemon {
+        public Charizad() {
+            super("Charizad", "Fire", 120, 270, 1.5, move0, move1, move2, move3, pokemon);
+
+        }
     }
 }
