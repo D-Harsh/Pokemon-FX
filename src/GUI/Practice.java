8ef56325382794844eb32e1b55f15c59209b0720
D-Harsh <harsh200014@gmail.com>
2018-01-10 12:08:24 -0500

    more gui

--- src/GUI/Practice.java
+++ src/GUI/Practice.java
@@ -22,9 +22,28 @@ public class Practice extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Pokemon");
 		Button pok1 = new Button(), pok2 = new Button(), pok3 = new Button();
-		pok1.setText("Charmaleon");
-		pok2.setText("Charmeleon");
-		pok3.setText("Charizard");
+//		pok1.setText("Charmaleon");
+		pok1.setStyle(
+                "-fx-background-radius: 5em; " +
+                "-fx-min-width: 50px; " +
+                "-fx-min-height: 50px; " +
+                "-fx-max-width: 50px; " +
+                "-fx-max-height: 50px;"
+        );
+		pok2.setStyle("-fx-background-radius: 5em; " +
+                "-fx-min-width: 50px; " +
+                "-fx-min-height: 50px; " +
+                "-fx-max-width: 50px; " +
+                "-fx-max-height: 50px;"
+                );
+		pok3.setStyle("-fx-background-radius: 5em; " +
+                "-fx-min-width: 50px; " +
+                "-fx-min-height: 50px; " +
+                "-fx-max-width: 50px; " +
+                "-fx-max-height: 50px;"
+                );
+//		pok2.setText("Charmeleon");
+//		pok3.setText("Charizard");
 		Media media = new Media(
 				"file:/C:/Users/Harsh/Documents/School%20Stuff/Grade%2012/CompSci/Pokemon-FX/src/Images/Theme.mp3");
 		MediaPlayer player = new MediaPlayer(media);
@@ -35,13 +54,21 @@ public class Practice extends Application {
 				"file:/C:/Users/Harsh/Documents/School%20Stuff/Grade%2012/CompSci/Pokemon-FX/src/Images/2.gif");
 		Image img3 = new Image(
 				"file:/C:/Users/Harsh/Documents/School%20Stuff/Grade%2012/CompSci/Pokemon-FX/src/Images/3.gif");
+		Image img4 = new Image(
+				"file:/C:/Users/Harsh/Documents/School%20Stuff/Grade%2012/CompSci/Pokemon-FX/src/Images/pokeball.png");
 		Image[] list = { img1, img2, img3 };
-		ImageView field = new ImageView(), bg = new ImageView();
+		ImageView field = new ImageView(), bg = new ImageView(), pokeball = new ImageView(img4), pokeball1 = new ImageView(img4), pokeball2 = new ImageView(img4);
 		bg.setImage(new Image("file:/C:/Users/Harsh/Documents/School%20Stuff/Grade%2012/CompSci/Pokemon-FX/src/Images/bg.png"));
 		bg.setFitHeight(600);
 		bg.setFitWidth(1000);
 		bg.setPreserveRatio(true);
 		field.setImage(img1);
+		pokeball.setFitHeight(50);pokeball.setFitWidth(50);
+		pokeball1.setFitHeight(50);pokeball1.setFitWidth(50);
+		pokeball2.setFitHeight(50);pokeball2.setFitWidth(50);
+		pok1.setGraphic(pokeball);
+		pok2.setGraphic(pokeball1);
+		pok3.setGraphic(pokeball2);
 		pok1.setOnAction(e -> {
 			field.setImage(list[0]);
 		});
@@ -61,6 +88,7 @@ public class Practice extends Application {
 		primaryStage.show();
 
 	}
+
 
 	public void setCoordinates(Object a, int x, int y) {
 		((Node) a).setLayoutX(x);
