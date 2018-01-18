e951299c0e9ee34db95fd6aece5f89d4e73f7eac
D-Harsh <harsh200014@gmail.com>
2018-01-18 18:45:52 -0500

    choose scene

--- src/GUI/SelectPokemon.java
+++ src/GUI/SelectPokemon.java
@@ -12,8 +12,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import javax.xml.soap.Text;
-
 /**
  * Created by Harsh on 2018-01-17.
  */
@@ -48,45 +46,27 @@ public class SelectPokemon extends Application{
         setCoordinates(pokeball,350,400);
         setCoordinates(trainer1,30,100);
         setCoordinates(trainer2,700,100);
-
-
-
-
-
         ImageView background = new ImageView(new Image("Images/background.jpg"));
         background.setFitHeight(600);
         background.setFitWidth(965);
         Pane selectPane = new Pane();
         selectPane.getChildren().addAll(background, trainer1,trainer2,players,pokeball,pokefield);
-
-
-
         //text fields and buttons
         //Team 1 setup
         TextField[] team1 = new TextField[6];
         Button[] buttons1 = new Button[6];
+
         for (int i = 1; i <= 6; i++) {
             TextField tf = new TextField("Enter Pokedex Num");
-            String name = "Text field "+i ;
-            tf.setOnAction(e -> {
-                System.out.println("Action on "+name+": text is "+tf.getText());
-            });
+            tf.setOpacity(0.90);
             setCoordinates(tf, 50,250+(i*50));
-
-            Button pokemon = pokebutton();
-            pokemon.setOnAction(e->{
-                int num = Integer.parseInt(tf.getText());
-                for (Pokemon pok: pokedex){
-                    if (pok.getPokedexNum() == num){
-                        pokefield.setImage(pok.getPokemonImage());
-                    }
-                }
-            });
-            setCoordinates(pokemon, 200,250+(i*50));
-
-            selectPane.getChildren().addAll(tf,pokemon);
+            Button confirm = pokebutton();
+            setCoordinates(confirm, 200,250+(i*50));
+            tf.setOnAction(e-> checkPokedexNum(tf,pokefield));
+            confirm.setOnAction(e->checkPokedexNum(tf,pokefield));
+            selectPane.getChildren().addAll(tf,confirm);
             team1[i-1] = tf ;
-            buttons1[i-1] = pokemon;
+            buttons1[i-1] = confirm;
         }
 
         //Team 2 setup
@@ -94,35 +74,75 @@ public class SelectPokemon extends Application{
         Button[] buttons2 = new Button[6];
         for (int i = 1; i <= 6; i++) {
             TextField tf = new TextField("Enter Pokedex Num");
-            String name = "Text field "+i ;
-            tf.setOnAction(e -> {
-                System.out.println("Action on "+name+": text is "+tf.getText());
-            });
+            tf.setOpacity(0.90);
             setCoordinates(tf, 750,250+(i*50));
-
-            Button pokemon = pokebutton();
-            setCoordinates(pokemon, 725,250+(i*50));
-            pokemon.setOnAction(e->{
-                int num = Integer.parseInt(tf.getText());
-                for (Pokemon pok: pokedex){
-                    if (pok.getPokedexNum() == num){
-                        pokefield.setImage(pok.getPokemonImage());
-                    }
-                }
-            });
-            selectPane.getChildren().addAll(tf,pokemon);
+            Button confirm = pokebutton();
+            setCoordinates(confirm, 725,250+(i*50));
+            tf.setOnAction(e-> checkPokedexNum(tf,pokefield));
+            confirm.setOnAction(e->checkPokedexNum(tf,pokefield));
+            selectPane.getChildren().addAll(tf,confirm);
             team2[i-1] = tf ;
-            buttons2[i-1] = pokemon;
+            buttons2[i-1] = confirm;
         }
-
-
-
-
-
-
-
         primaryStage.setScene(new Scene(selectPane, 965, 600));
         primaryStage.show();
+    }
+    public Scene getChooseScene(){
+        ImageView trainer1 = new ImageView(new Image("Images/trainer1.gif"));
+        ImageView trainer2 = new ImageView(new Image("Images/trainer2.gif"));
+        ImageView pokeball = new ImageView(new Image("Images/pokeballanimated.gif"));
+        ImageView players = new ImageView(new Image("Images/p1p2.png"));
+        ImageView pokefield = new ImageView();
+        pokefield.setFitHeight(225);pokefield.setFitWidth(225);
+        pokefield.setPreserveRatio(true);
+        trainer2.setFitHeight(200);trainer2.setFitWidth(200);
+        trainer2.setPreserveRatio(true);
+        players.setFitWidth(400); players.setFitHeight(200);
+        players.setPreserveRatio(true);
+        setCoordinates(players,295,20);
+        setCoordinates(pokefield,380,200);
+        setCoordinates(pokeball,350,400);
+        setCoordinates(trainer1,30,100);
+        setCoordinates(trainer2,700,100);
+        ImageView background = new ImageView(new Image("Images/background.jpg"));
+        background.setFitHeight(600);
+        background.setFitWidth(965);
+        Pane selectPane = new Pane();
+        selectPane.getChildren().addAll(background, trainer1,trainer2,players,pokeball,pokefield);
+        //text fields and buttons
+        //Team 1 setup
+        TextField[] team1 = new TextField[6];
+        Button[] buttons1 = new Button[6];
+
+        for (int i = 1; i <= 6; i++) {
+            TextField tf = new TextField("Enter Pokedex Num");
+            tf.setOpacity(0.90);
+            setCoordinates(tf, 50,250+(i*50));
+            Button confirm = pokebutton();
+            setCoordinates(confirm, 200,250+(i*50));
+            tf.setOnAction(e-> checkPokedexNum(tf,pokefield));
+            confirm.setOnAction(e->checkPokedexNum(tf,pokefield));
+            selectPane.getChildren().addAll(tf,confirm);
+            team1[i-1] = tf ;
+            buttons1[i-1] = confirm;
+        }
+
+        //Team 2 setup
+        TextField[] team2 = new TextField[6];
+        Button[] buttons2 = new Button[6];
+        for (int i = 1; i <= 6; i++) {
+            TextField tf = new TextField("Enter Pokedex Num");
+            tf.setOpacity(0.90);
+            setCoordinates(tf, 750,250+(i*50));
+            Button confirm = pokebutton();
+            setCoordinates(confirm, 725,250+(i*50));
+            tf.setOnAction(e-> checkPokedexNum(tf,pokefield));
+            confirm.setOnAction(e->checkPokedexNum(tf,pokefield));
+            selectPane.getChildren().addAll(tf,confirm);
+            team2[i-1] = tf ;
+            buttons2[i-1] = confirm;
+        }
+        return new Scene(selectPane, 965, 600);
     }
     public void setCoordinates(Object a, int x, int y) {
         ((Node) a).setLayoutX(x);
@@ -139,5 +159,23 @@ public class SelectPokemon extends Application{
         pok1.setGraphic(pokeball);
         return pok1;
     }
-
+    public boolean checkPokedexNum(TextField choice, ImageView pokefield){
+        try{
+            int num = Integer.parseInt(choice.getText());
+            if (num > 151 || num < 1){
+                choice.setText("Invalid Input");
+                return false;
+            }
+            for (Pokemon pok : pokedex) {
+                if (pok.getPokedexNum() == num) {
+                    pokefield.setImage(pok.getPokemonImage());
+                }
+            }
+            return true;
+        }
+        catch(IllegalArgumentException  e){
+            choice.setText("Invalid Input");
+            return false;
+        }
+    }
 }
